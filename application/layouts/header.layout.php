e1d0fc35547f8e9132213dfea3a8a4e3fb0d0d7f
Xavier Masson <Masson.Xavier.91@gmail.com>
2012-11-07 21:26:54 +0100

    Add Ajax connexion + Structure code

--- application/layouts/header.layout.php
+++
@@ -1,8 +0,0 @@
-<head>
-    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-    <title>Petites Annonces</title>
-    <!-- Bootstrap -->
-    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
-    <link href="css/all.css" rel="stylesheet">
-    <!-- <link rel="stylesheet" type="text/css" href="css/all.css"> -->
-</head>
