0704e4f21c3ae5016adf44a0dc4b3f3e35ccc938
Ritooon <henri.vignaux@gmail.com>
2013-01-31 18:49:47 +0100

    Some changes

--- application/views/Member/message.view.php
+++ application/views/Member/message.view.php
@@ -6,27 +6,27 @@
         </header>
         <div class="container"> 
             <div class="row">
-                <div class="span7 well">
+                <div class="span10 well">
                     <h4 class="blue">Le 12/12/2012 à 12h12, Darth Vader a écrit :</h4>
                     <hr />
                     Bonjour,<br />
                     J'en ai absolument besoin, je serais heureux de pouvoir l'acquérir ...<br />
                     Cordialement.
                 </div>
-                <div class="span7 well">
+                <div class="span10 well">
                     <h4 class="blue">Le 12/12/2012 à 14h15, Vous avez écrit :</h4>
                     <hr />
                     Bonjour,<br />
                     J'ai beaucoup de demande pour ce sabre. Que me proposez-vous en échange ?<br />
                     Cordialement.
                 </div>
-                <div class="span7 well">
+                <div class="span10 well">
                     <h4 class="blue">Le 12/12/2012 à 16h36, Darth Vader a écrit :</h4>
                     <hr />
                     La vie ...<br />
                     Cordialement.
                 </div>
-                <form class="span7 well">
+                <form method="post" action="#" class="span10 well">
                     <h4 class="blue">Répondre</h4>
                     <textarea name="response" id="response" rows="6" cols="80" placeholder="..."></textarea>
                     <br />
